d03947fe6180fb5208f6114b615e0bed09d3a1be
ankit <ankit.mishra@bharatpe.com>
2021-02-08 15:34:53 +0530

    config file

--- src/main/java/com/bharatpe/cache/CacheApplication.java
+++ src/main/java/com/bharatpe/cache/CacheApplication.java
@@ -2,8 +2,14 @@ package com.bharatpe.cache;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+@PropertySources({
+		@PropertySource("file:/etc/bharatpe/key.properties"),
+		@PropertySource("file:/etc/bharatpe/production.properties")
+})
 public class CacheApplication {
 
 	public static void main(String[] args) {
